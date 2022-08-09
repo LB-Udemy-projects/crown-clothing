fbd14a726a7b1e990c065cd065b4fb7f9aaeee27
Leonid Baidulin <lbajdull@gmail.com>
2022-08-09 15:38:29 +0300

    install redux-thunk and use it in the categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,14 +3,18 @@ import { useSelector } from 'react-redux/es/exports';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading,
+} from '../../store/categories/category.selector';
 
 import './category.styles.scss';
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
-
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -20,12 +24,16 @@ const Category = () => {
   return (
     <>
       <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className='category-container'>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
     </>
   );
 };
